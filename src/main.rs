c544793133eea64d7d91147d294ea84f9e07c6c8
Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>
2021-02-12 15:57:40 +0000

    Starting benchmark - has 99% load balancing, really good.

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+extern crate nalgebra;
+//use nalgebra::Vector3;
+use rand::distributions::{Distribution, Normal};
+use specs::{Builder, World, VecStorage, System, Component, DispatcherBuilder, ReadStorage, WriteStorage};
+
+pub struct ComponentA {
+    pub value: f64,
+}
+impl Component for ComponentA {
+    type Storage = VecStorage<Self>;
+}
+
+pub struct ComponentB {
+    pub value: f64,
+}
+impl Component for ComponentB {
+    type Storage = VecStorage<Self>;
+}
+
+pub struct ComponentC {
+    pub value: f64,
+}
+impl Component for ComponentC {
+    type Storage = VecStorage<Self>;
+}
+
+
+pub struct SimpleSystem;
+impl<'a> System<'a> for SimpleSystem {
+	type SystemData = (
+		ReadStorage<'a, ComponentA>,
+        ReadStorage<'a, ComponentB>,
+        WriteStorage<'a, ComponentC>,
+	);
+
+	fn run(&mut self, (a, b, mut c): Self::SystemData) {
+        use rayon::prelude::*;
+        use specs::ParJoin;
+
+        (&a, &b, &mut c).par_join().for_each(|(a,b,mut c)| {
+            c.value = a.value * b.value + a.value;    
+        });
+    }
+}
+
+fn main() {
+
+    let mut world = World::new();
+    let mut builder = DispatcherBuilder::new();
+
+    builder.add(SimpleSystem, "simple_system", &[]);
+
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(6)
+        .build()
+        .unwrap();
+
+    builder.add_pool(::std::sync::Arc::new(pool));
+    let mut dispatcher = builder.build();
+    dispatcher.setup(&mut world.res);
+
+    // Add test entities
+    let mut rng = rand::thread_rng();
+    let dist = Normal::new(0.0, 1.0);
+
+    for _ in 0..1000000 {
+        world
+            .create_entity()
+            .with(ComponentA {
+                value:
+                    dist.sample(&mut rng),
+            })
+            .with(ComponentB {
+                value:
+                    dist.sample(&mut rng),
+            })
+            .with(ComponentC {
+                value:
+                    0.0,
+            })
+            .build();
+    }
+
+    for _ in 0..10000 {
+        dispatcher.dispatch(&mut world.res);
+        world.maintain();
+    }
+}
